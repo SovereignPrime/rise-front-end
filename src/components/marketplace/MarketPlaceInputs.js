8434449305c8e769c1cd62d0c35d6c74dbf5880a
Mingyu Chen <chen_mingyu416@sina.com>
2022-05-23 17:16:40 -0400

    add some component in the market place

--- src/components/marketplace/MarketPlaceInputs.js
+++ src/components/marketplace/MarketPlaceInputs.js
@@ -8,6 +8,8 @@ import prod1 from '../../styles/assets/appwatch.jpg';
 
 import './MarketPlaceInputs.css';
 
+import "bootstrap/dist/css/bootstrap.css";
+
 const data = [
      {
           PerName: "Eric Smith",
@@ -30,121 +32,87 @@ const data = [
           good: 14,
           bad: 2
 
-     }
+     },
+
 ];
 
 const MarketPlaceInputs = () => {
      return (
           <div className='marketPlace'>
-               <Row>
-                    <Col md={6}>
-                         <h3>category</h3>
-                         <select>
-                              <option>All Categories</option>
-                              <option value="vehicles">Vehicles</option>
-                              <option value="property-rental">Property Rental</option>
-                              <option value="apparel">Apparel</option>
-                              <option value="classifieds">Classifieds</option>
-                              <option value="electronics">Electronics</option>
-                              <option value="entertainment">Entertainment</option>
-                              <option value="family">Family</option>
-                              <option value="free stuff">Free Stuff</option>
-                              <option value="garden/outdoors">Garden & Outdoors</option>
-                              <option value="other">Other</option>
+               <Col md={3}>
+                    <h3>category</h3>
+                    <select>
+                         <option>All Categories</option>
+                         <option value="vehicles">Vehicles</option>
+                         <option value="property-rental">Property Rental</option>
+                         <option value="apparel">Apparel</option>
+                         <option value="classifieds">Classifieds</option>
+                         <option value="electronics">Electronics</option>
+                         <option value="entertainment">Entertainment</option>
+                         <option value="family">Family</option>
+                         <option value="free stuff">Free Stuff</option>
+                         <option value="garden/outdoors">Garden & Outdoors</option>
+                         <option value="other">Other</option>
 
-                         </select>
-
-
-                         <select>
-                              <option >Sort by</option>
-                              <option value="price">Price</option>
-                              <option value="Year">Year</option>
-
-                         </select>
+                    </select>
 
 
-                    </Col>
-                    <Col md={6}>
-                         <h3>product</h3>
-                         {/* <div className='test'>
-                              <div >
-                                   <img src={img1}></img>
-                              </div>
-                              <div >
-                                   <img src={img1}></img>
-                              </div>
-                              <div >
-                                   <img src={img1}></img>
-                              </div>
-                              <div >
-                                   <img src={img1}></img>
-                              </div>
-                              <div >
-                                   <img src={img1}></img>
-                              </div>
-                              <div >
-                                   <img src={img1}></img>
-                              </div>
-                              <div >
-                                   <img src={img1}></img>
-                              </div>
-                              <div >
-                                   <img src={img1}></img>
-                              </div>
-                              <div >
-                                   <img src={img1}></img>
-                              </div>
-                              <div >
-                                   <img src={img1}></img>
-                              </div>
+                    <select>
+                         <option >Sort by</option>
+                         <option value="price">Price</option>
+                         <option value="Year">Year</option>
 
-                         </div> */}
+                    </select>
 
 
-                         <div className='card'>
+               </Col>
+
+               <Col md={9}>
+                    <div class="container">
+                         <div class="row">
                               {
-                                   data.map(item => (
-                                        <Card className='cardItem'>
-                                             <CardImg
-                                                  alt="Card image cap"
-                                                  src={item.prodPic}
-                                                  width="80%"
-                                                  top
-                                             />
-                                             <CardBody >
-                                                  <CardText >
-                                                       <div className="market-person">
-                                                            <img src={item.PerPic} width="20%"></img>
+                                   data.map((item) => (
+                                        <div class="col-3">
+                                             <Card >
+                                                  <CardImg
+                                                       alt="Card image cap"
+                                                       src={item.prodPic}
+                                                       width="30%"
+                                                       top
+                                                  />
+                                                  <CardBody >
+                                                       <CardText >
+                                                            <div className="market-person">
+                                                                 <img src={item.PerPic} width="20%"></img>
 
-                                                            <small className="text-muted">
-                                                                 &nbsp; {item.PerName}
-                                                            </small>
-                                                       </div>
-
-
-                                                       <div className='market-product-name'>
-                                                            {item.prodName}
-                                                       </div>
-                                                       <div >
-
-                                                            <Row md="2">
-                                                                 <Col className='market-product-price'>
-                                                                      {item.Price}
-                                                                 </Col>
-                                                                 <Col className='market-product-detail-btn'>
-                                                                      <Button >
-                                                                           Detail
-                                                                      </Button>
-                                                                 </Col></Row>
-                                                       </div>
-                                                  </CardText >
-                                             </CardBody>
-                                        </Card>
+                                                                 <small className="text-muted">
+                                                                      &nbsp; {item.PerName}
+                                                                 </small>
+                                                            </div>
+                                                            <div className='market-product-name'>
+                                                                 {item.prodName}
+                                                            </div>
+                                                            <div >
+                                                                 <Row md="2">
+                                                                      <Col className='market-product-price'>
+                                                                           {item.Price}
+                                                                      </Col>
+                                                                      <Col className='market-product-detail-btn'>
+                                                                           <Button >
+                                                                                Detail
+                                                                           </Button>
+                                                                      </Col></Row>
+                                                            </div>
+                                                       </CardText >
+                                                  </CardBody>
+                                             </Card>
+                                        </div>
                                    ))
                               }
                          </div>
-                    </Col>
-               </Row>
+                    </div>
+               </Col>
+
           </div>
 
      );
