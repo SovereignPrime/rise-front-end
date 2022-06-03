66abe1047434c669dd0879b524cc236372b05960
Mingyu Chen <chen_mingyu416@sina.com>
2022-06-03 10:34:04 -0400

    save me/marketplace

--- src/components/Me/MeComponents/marketPlace.js
+++ src/components/Me/MeComponents/marketPlace.js
@@ -1,30 +1,98 @@
-import React, {Component} from "react";
-import {Card, CardImg, CardText, CardTitle, Row, Col} from "reactstrap";
+import React, { Component } from "react";
+import { Card, CardImg, CardText, CardTitle, Row, Col } from "reactstrap";
+import img1 from '../../../styles/assets/img/jessie.png';
+import prod1 from '../../../styles/assets/appwatch.jpg';
+import prod2 from '../../../styles/assets/minicooper.jpeg';
+import prod3 from '../../../styles/assets/ps5.jpeg';
 
+const data = [
+    {
+        Id: 1,
+        perName: "Eric Smith",
+        perPic: img1,
+        prodName: "Apple Watch",
+        Category: "electronics",
+        Price: 450,
+        Detail: "This is a brand new apple Watch. I used it onece, but then found I do not  need it. I is in very good condition",
+        prodPic: prod1,
+        Good: 14,
+        Bad: 2,
+        Date: "2022-05-25"
+    },
+    {
+        Id: 2,
+        perName: "Eric Smith",
+        perPic: img1,
+        prodName: "MINI cooper",
+        Category: "vehicles",
+        Price: 1000,
+        Detail: "this is a car",
+        prodPic: prod2,
+        Good: 14,
+        Bad: 2,
+        Date: "2022-05-22"
+    },
+    {
+        id: 3,
+        perName: "Eric Smith",
+        perPic: img1,
+        prodName: "PS5",
+        Category: "entertainment",
+        Price: 200,
+        Detail: "this is a PS station",
+        prodPic: prod3,
+        Good: 14,
+        Bad: 2,
+        Date: "2021-05-27"
+    },
+];
 class MarketPlace extends Component {
+    constructor(props) {
+        super(props);
 
-    render(){
-        return(
-                <Card className="lg mx-3 my-3">
-                <div className="mx-3 my-3">
-                <CardTitle className="border-bottom">
-                    <Row>
-                        <Col className="text-start mx-3">To xxx</Col> 
-                        <Col className="text-end mx-3">date</Col>
-                    </Row>
-                </CardTitle>
-                <CardText className="text-start mx-3">
-                    something
-                </CardText>
-                <CardImg
-                    className="mx-3 my-3"
-                    src="#"
-                    alt="img"
-                    width="30%"
-                    bottom
-                />
-                </div>
-            </Card>
+        this.state = {
+            data
+        }
+    }
+
+    render() {
+
+        return (
+            <>
+                {this.state.data.map((item) => {
+                    return (
+                        <Card className="lg mx-3 my-3">
+                            <div className="mx-3 my-3">
+                                <CardTitle className="border-bottom">
+                                    <Row>
+                                        <Col className="text-start mx-3" style={{ fontWeight: "bold", fontSize: 20 }}>{item.prodName}</Col>
+                                        <Col className="text-end mx-3" style={{ fontSize: 20 }}>${item.Price}</Col>
+                                    </Row>
+                                </CardTitle>
+                                <CardText className="text-start mx-3" style={{ fontSize: 18 }}>
+                                    {item.Detail}
+                                </CardText>
+                                <Row xs="2" >
+                                    <Col className="mx-1 text-start mx-3" >
+                                        <CardImg
+                                            style={{ width: "80%" }}
+                                            className="border"
+                                            src={item.prodPic}
+                                            alt="img"
+                                        />
+
+                                    </Col>
+
+                                </Row>
+
+
+                            </div>
+                        </Card>
+                    )
+                })
+                }
+            </>
+
         )
     }
 }
