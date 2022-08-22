735c77f6e39e4b120c6e459d284239e73fb6eae4
Mingyu Chen <chen_mingyu416@sina.com>
2022-08-22 17:12:56 -0400

    solve conflict

--- src/components/Card/CardObject.js
+++ src/components/Card/CardObject.js
@@ -47,48 +47,6 @@ const CardObject = (props) => {
     setCheckSellerInfo(false);
   };
 
-<<<<<<< HEAD
-	return (
-		<div>
-			<Routes>
-				<Route
-					path="/"
-					element={
-						<div className="containter">
-							<div className="row">
-								{checkSellerInfo && (
-									<CheckSellerInfo
-										closeCheckSellerHandler={closeCheckSellerHandler}
-										checkSellerName={sellerName}
-										checkSellerPic={sellerPic}
-									/>
-								)}
-								{data?.map((item) => (
-									<div key={item.Id} className="col-lg-4 col-md-6 col-sm-12">
-										<Card key={item.Id}>
-											<CardImg
-												className="cardimg"
-												alt="Card image cap"
-												src={item.prodPic}
-												width="20%"
-												height={200}
-												top
-											/>
-											<CardBody>
-												<CardText>
-													<div className="market-person">
-														<img className="market-person-photo" src={item.perPic} width="15%"></img>
-														{/* <small className="text-muted">&nbsp; {item.perName}</small> */}
-														<small
-															className="text-muted"
-															onClick={() => {
-																setCheckSellerInfo(true);
-																setSellerName(item.perName);
-																setSellerPic(item.perPic);
-															}}>
-															&nbsp; {item.perName}
-														</small>
-=======
   return (
     <div>
       <Routes>
@@ -135,7 +93,6 @@ const CardObject = (props) => {
                             >
                               &nbsp; {item.perName}
                             </small>
->>>>>>> devBranch
 
                             <FontAwesomeIcon
                               className="iconN"
