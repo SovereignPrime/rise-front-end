862334df342cab546893e3926a9481aa18d5a585
ChloeGwon <chgmlk@gmail.com>
2022-08-24 11:24:32 -0700

    verificationStatus
    verificationStatus

--- src/components/Me/MeComponents/verificationStatus.js
+++ src/components/Me/MeComponents/verificationStatus.js
@@ -1,28 +1,62 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import { Card, Row, Col } from "reactstrap";
-import PieChart from "../../PieChart/PieChart";
+import { Tooltip, BarChart, XAxis, YAxis, Legend, CartesianGrid, Bar } from "recharts";
 
-class VerificationStatus extends Component {
-	render() {
-		return (
-			<div className="px-3 py-3">
-				<Row className="row-lg-1">
-					<Col className="col-12 border text-start">Authitic User</Col>
-				</Row>
-				<Row className="row-4 py-3">
-					<Col className="col-7 border me-5">2</Col>
-					<Col className="col-4">
-						<Row className="border py-5 mb-3">3</Row>
-						<Row className="border py-5"></Row>
-					</Col>
-				</Row>
-				<Row className="row-2">
-					<Col className="border">5</Col>
-				</Row>
-				<PieChart />
-			</div>
-		);
-	}
+function VerificationStatus() {
+    const data = [
+        {name: 'Mon', value: 30},
+        {name: 'Tue', value: 40},
+        {name: 'Wed', value: 50},
+        {name: 'Thu', value: 60},
+        {name: 'Fri', value: 70},
+        {name: 'Sat', value: 80},
+        {name: 'Sun', value: 90}
+    ];
+
+    return(
+        <div className="px-3 py-3">
+            <Row className="row-lg-1">
+                <Col className="col-12 border text-start">Authitic User</Col>
+            </Row>
+            <Row className="row-4 py-3">
+                <Col className="col-7 border me-5">2</Col>
+                <Col className="col-4">
+                    <Row className="border mb-3">
+                        <p style={{margin: 0, fontSize: 45, fontWeight: "bold"}}>23</p>
+                        <p style={{fontSize: 15, color: "#bfbdbd", fontWeight: "700", marginTop: -15}}>User Verified</p>
+                    </Row>
+                    <Row className="border py-3">
+                        <p style={{fontSize: 13, fontWeight: "bold", textAlign: "start"}}>Weekly Verification Number</p>
+                        <BarChart
+                                width={330}
+                                height={230}
+                                data={data}
+                                // margin={{
+                                //     top: 5,
+                                //     right: 35,
+                                //     left: 5,
+                                //     bottom: 5,
+                                // }}
+                                
+                                barSize={18}
+                            >
+                            <XAxis dataKey="name" padding={{ left: 10, right: 10 }} fontSize={15} fontStyle={'oblique'} ticks={{display:false}}/>
+                            {/* <Tooltip  /> */}
+                            {/* <Legend /> */}
+                            {/* <CartesianGrid strokeDasharray="3 3" /> */}
+                            <Bar dataKey="value" fill="#99ffdd" background={{ fill: '#eee' }} />
+                        </BarChart>
+                    </Row>
+                </Col>
+            </Row>
+            <Row className="row-2">
+                <Col className="border text-start py-4">
+                    <h4>5 Verification Left This Week</h4>
+                </Col>
+            </Row>
+
+        </div>
+    )
 }
 
 export default VerificationStatus;
