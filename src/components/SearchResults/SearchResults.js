8826c975a42d4245f85901bcec2bf835d59a64ee
PilotPrix <69162332+PilotPrix@users.noreply.github.com>
2022-12-10 00:32:34 -0500

    Search functionality works!
    Approximating for misspellings also work

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -5,6 +5,7 @@ import img1 from "../../styles/assets/img/jessie.png";
 import prod1 from "../../styles/assets/appwatch.jpg";
 import prod2 from "../../styles/assets/minicooper.jpeg";
 import prod3 from "../../styles/assets/ps5.jpeg";
+import { isConstructorDeclaration } from 'typescript';
 
 let results = [
     {
@@ -47,13 +48,42 @@ let results = [
         Date: "2021-05-27",
     },
 ];
+
+// Search algorithm approximation for misspellings
+const levenshteinDistance = (str1 = '', str2 = '') => {
+    const track = Array(str2.length + 1).fill(null).map(() =>
+    Array(str1.length + 1).fill(null));
+    for (let i = 0; i <= str1.length; i += 1) {
+       track[0][i] = i;
+    }
+    for (let j = 0; j <= str2.length; j += 1) {
+       track[j][0] = j;
+    }
+    for (let j = 1; j <= str2.length; j += 1) {
+       for (let i = 1; i <= str1.length; i += 1) {
+          const indicator = str1[i - 1] === str2[j - 1] ? 0 : 1;
+          track[j][i] = Math.min(
+             track[j][i - 1] + 1, // deletion
+             track[j - 1][i] + 1, // insertion
+             track[j - 1][i - 1] + indicator, // substitution
+          );
+       }
+    }
+    return track[str2.length][str1.length];
+  };
+
 const SearchResults = (props) => {
     // properties: searchTerm, setSearchTerm
 
     return (
         <div className="container">
             <h1>Search Results for: {props.searchTerm}</h1>
-            {results.map((result, i) => {
+            {results.filter(item => {
+                const distance = levenshteinDistance(item.prodName.toLowerCase(), props.searchTerm);
+                console.log("Distance: " + distance);
+                return distance <= 3;
+            })
+            .map((result, i) => {
                 return (
                     <div className="result">
                         <div className="profile">
